15e31d76a5741127bdd0b025992f048a75726416
Danrley Dimas <danrleydimas@outlook.com>
2024-07-05 17:19:03 -0300

    update

--- src/components/Carrosel-1.js
+++ src/components/Carrosel-1.js
@@ -8,45 +8,47 @@ import logoruby from '../assets/logo-ruby.svg'
 import '../styles/carrosel1.css'
 
 export default function() {
-    return (
-        <div className="anuncios-container">
-            <div className="anuncio-1">
-                <img src={pythonBurguer} alt="Burguer Artesanal Python"/>
-                <div className="anuncio-1-text"> 
-                    <h3><img src={logopython} alt="logo da Python" />Python</h3>
-                    <p>Pão de alto nível, Hamburguer de carne de urso,
-                    com aquele sabor forte e para dar aquela dinamica,
-                    Queijo Prato, Salada e maionese verde.</p>
-                    <p className="promo-fritas">Acompanha Fritas</p>
-                    <button className="button-pedido">Pedir Agora</button>
-                    <p>R$ 34,90</p>
-                </div>
-            </div>
-                {/* <div className="anuncio-2">
-                    <img src={javaBurguer} alt="Burguer Artesanal Java"/>
-                    <img src={logojava} alt="logo do Java" />
-                    <h3>Java</h3>
-                    <p>
-                    Pão de Hamburguer simples, 2 hamburguers de 70 grama,
-                    alface, tomate e maionese,um lanche humilde mas,
-                    excelente e se achar pouco pode complementar.
-                    </p>
-                    <p>Acompanha Fritas</p>
-                    <p>R$ 28,90</p>
+    return // (
+    //    <div className="box-main">
+    //         <div className="box-anuncio">
+    //             <img src={pythonBurguer} alt="Burguer Artesanal Python" className="python-burguer"/>
+    //             <div> 
+    //                 <h3><img src={logopython} alt="logo da Python" />Python</h3>
+    //                 <p>Pão de alto nível, Hamburguer de carne de urso,
+    //                 com aquele sabor forte e para dar aquela dinamica,
+    //                 Queijo Prato, Salada e maionese verde.</p>
+    //                 <p>Acompanha Fritas</p>
+    //                 <button>Pedir Agora</button>
+    //                 <div>
+    //                     <p>R$ 34,90</p>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //             <div className="anuncio-2">
+    //                 <img src={javaBurguer} alt="Burguer Artesanal Java"/>
+    //                 <img src={logojava} alt="logo do Java" />
+    //                 <h3>Java</h3>
+    //                 <p>
+    //                 Pão de Hamburguer simples, 2 hamburguers de 70 grama,
+    //                 alface, tomate e maionese,um lanche humilde mas,
+    //                 excelente e se achar pouco pode complementar.
+    //                 </p>
+    //                 <p>Acompanha Fritas</p>
+    //                 <p>R$ 28,90</p>
 
-                </div>
-                <div className="anuncio-3">
-                    <img src={rubyBurguer} alt="Burguer Artesanal Ruby"/>
-                    <img src={logoruby} alt="logo do Ruby" />
-                    <h3>ruby</h3>
-                    <p>
-                    Pão Italiano, presunto parma, rúcula e brócolis,
-                    um sandwich dinâmico que possuí um gosto forte
-                    e marcante que automaticamente vai ficar na sua memória.
-                    </p>
-                    <p>Acompanha Fritas</p>
-                    <p>R$ 29,90</p>
-                </div> */}
-        </div>
-    )
+    //             </div>
+    //             <div className="anuncio-3">
+    //                 <img src={rubyBurguer} alt="Burguer Artesanal Ruby"/>
+    //                 <img src={logoruby} alt="logo do Ruby" />
+    //                 <h3>ruby</h3>
+    //                 <p>
+    //                 Pão Italiano, presunto parma, rúcula e brócolis,
+    //                 um sandwich dinâmico que possuí um gosto forte
+    //                 e marcante que automaticamente vai ficar na sua memória.
+    //                 </p>
+    //                 <p>Acompanha Fritas</p>
+    //                 <p>R$ 29,90</p>
+    //             </div>
+    //     </div>
+    // )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,20 +2,34 @@ import '../styles/header.css';
 import logoburguer from '../assets/logo-burguer.svg';
 import phone from '../assets/phone.svg'
 import carrinho from '../assets/shopcart.svg'
+import { useState } from 'react';
 
 export default function NavBar() {
+    const [isOpen, SetisOpen] = useState(false)
+    
+    function toggleMenu() {
+        SetisOpen(!isOpen);
+    }
+
     return (
-            <header className="cabecalho">
-                <h1>CodeXburguer</h1>
-                <img src={logoburguer} alt="logo da hamburgueria" className='logo' />
-                <nav className="navbar">
-                    <a href="#" className="nav-item">Inicio</a>
-                    <a href="#" className="nav-item">Quem Somos</a>
-                    <a href="#" className="nav-item">Novidades</a>
-                    <a href="#" className="nav-item">Cardápio</a>
-                    <a href="#" className="delivery">Delivery<img src={phone} alt="Icone de delivery" className='phone-icone' /></a>
-                    <a href="#"><img src={carrinho} alt="Carrinho de itens selecionados" className='cart'/></a>
-                </nav>
+            <header className="box-principal">
+                <div className='box-'>
+                    <h1>CodeXburguer</h1>
+                </div>
+                <img src={logoburguer} alt="logo da hamburgueria"/>
+                    <button onClick={toggleMenu}>
+                        ☰
+                    </button>
+                    {isOpen && (<nav>
+                        <ul>
+                            <li><a href="#">Inicio</a></li>
+                            <li><a href="#">Quem Somos</a></li>
+                            <li><a href="#">Novidades</a></li>
+                            <li><a href="#">Cardápio</a></li>
+                            <li><a href="#">Delivery<img src={phone} alt="Icone de delivery"/></a></li>
+                        </ul>
+                    </nav>) }
+                    <a href="#"><img src={carrinho} alt="Carrinho de itens selecionados"/></a>
             </header>
     )
 }
